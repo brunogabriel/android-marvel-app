b2acc1930c6cf249b87692e5619e0be741da6d2d
Bruno Gabriel <bruno.gabriel@xpi.com.br>
2020-09-06 21:23:50 -0300

    Created new module components and starting new card item. Some layout changes.

--- MarvelApp/buildSrc/src/main/java/dependencies/Modules.kt
+++ MarvelApp/buildSrc/src/main/java/dependencies/Modules.kt
@@ -4,6 +4,7 @@ object Modules {
     const val STYLEGUIDE = ":styleguide"
     const val NETWORK = ":network"
     const val COMMONS = ":commons"
+    const val COMPONENTS = ":components"
 
     // features
     const val MAIN = ":features:main"
